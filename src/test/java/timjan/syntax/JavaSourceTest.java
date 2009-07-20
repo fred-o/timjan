73f4528ce82468dcd86801b546010d651e759f36
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2009-07-20 11:52:45 +0200

    Fixed compilation under java 1.5

--- src/test/java/timjan/syntax/JavaSourceTest.java
+++ src/test/java/timjan/syntax/JavaSourceTest.java
@@ -1,35 +1,30 @@
 package timjan.syntax;
 
 import java.text.*;
-import java.text.spi.*;
 import java.util.*;
+import java.util.concurrent.*;
+
 import junit.framework.*;
 
 public class JavaSourceTest extends TestCase {
-
 	public void testResolvesTo() throws Exception {
-		PackageStatement ps = new PackageStatement(new QualifiedIdentifier(
-					Arrays.asList("test", "classes")));
-		List<ImportStatement> imports = Arrays.asList(
-			new ImportStatement(new QualifiedIdentifier(Arrays.asList("java", "util", "List")), false, false), 
-			new ImportStatement(new QualifiedIdentifier(Arrays.asList("java", "text")), false, true));
-		List<Object> definitions = Arrays.<Object>asList(
-			new ClassDefinition(Arrays.<Modifier>asList(Visibility.PUBLIC), "SimpleClass1", Collections.<ClassReference>emptyList(), 
-					Collections.<ClassReference>emptyList(), Collections.<AbstractMember>emptyList()));
-	    JavaSource js = new JavaSource(ps, imports, definitions);
-		
+		PackageStatement ps = new PackageStatement(new QualifiedIdentifier(Arrays.asList("test", "classes")));
+		List<ImportStatement> imports = Arrays.asList(new ImportStatement(new QualifiedIdentifier(Arrays.asList("java",
+				"text", "Format")), false, false), new ImportStatement(new QualifiedIdentifier(Arrays.asList("java",
+				"util")), false, true));
+		List<Object> definitions = Arrays.<Object> asList(new ClassDefinition(Arrays
+				.<Modifier> asList(Visibility.PUBLIC), "SimpleClass1", Collections.<ClassReference> emptyList(),
+				Collections.<ClassReference> emptyList(), Collections.<AbstractMember> emptyList()));
+		JavaSource js = new JavaSource(ps, imports, definitions);
 		assertFalse(js.resolvesTo(new ClassReference("class.NonExistant", null), List.class));
-		assertTrue(js.resolvesTo(new ClassReference("java.util.List", null), List.class));
-		assertTrue(js.resolvesTo(new ClassReference("List", null), List.class));
-		assertFalse(js.resolvesTo(new ClassReference("LinkedList", null), List.class));
-		assertFalse(js.resolvesTo(new ClassReference("bogus.Class.List", null), List.class));
-
-		assertTrue(js.resolvesTo(new ClassReference("SimpleDateFormat", null), SimpleDateFormat.class));
-		assertFalse(js.resolvesTo(new ClassReference("Format", null), SimpleDateFormat.class));
+		assertTrue(js.resolvesTo(new ClassReference("java.text.Format", null), Format.class));
 		assertTrue(js.resolvesTo(new ClassReference("Format", null), Format.class));
-		assertFalse(js.resolvesTo(new ClassReference("CollatorProvider", null), CollatorProvider.class));
-		
+		assertFalse(js.resolvesTo(new ClassReference("SimpleDateFormat", null), Format.class));
+		assertFalse(js.resolvesTo(new ClassReference("bogus.Class.Format", null), Format.class));
+		assertTrue(js.resolvesTo(new ClassReference("java.util.List", null), List.class));
+		assertFalse(js.resolvesTo(new ClassReference("LinkedList", null), List.class));
+		assertTrue(js.resolvesTo(new ClassReference("List", null), List.class));
+		assertFalse(js.resolvesTo(new ClassReference("Executors", null), Executors.class));
 		assertTrue(js.resolvesTo(new ClassReference("SimpleClass1", null), test.classes.SimpleClass1.class));
 	}
-    
 }
